d8f933d62f7708aaa96445d1910938d7927a4439
-/\-MK-/\- <78851561+shunakiyama@users.noreply.github.com>
2025-05-04 17:50:13 +0200

    v3

---
+++ code/v3_multiple_sensors/v3_multiple_sensors.ino
@@ -0,0 +1,99 @@
+/*********
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com  
+*********/
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+#define ONE_WIRE_BUS 16 // Data wire is plugged TO GPIO 4
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+
+// Number of temperature devices found
+int numberOfDevices;
+
+// We'll use this variable to store a found device address
+DeviceAddress tempDeviceAddress; 
+
+void setup(){
+  // start serial port
+  Serial.begin(115200);
+  
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  delay(2000);
+
+  display.clearDisplay();
+  display.setTextSize(3);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 10);
+
+  // Start up the library
+  sensors.begin();
+  
+  // Grab a count of devices on the wire
+  numberOfDevices = sensors.getDeviceCount();
+  
+  // locate devices on the bus
+  Serial.print("Locating devices...");
+  Serial.print("Found ");
+  Serial.print(numberOfDevices, DEC);
+  Serial.println(" devices.");
+
+  // Loop through each device, print out address
+  for(int i=0;i<numberOfDevices; i++){
+    // Search the wire for address
+    if(sensors.getAddress(tempDeviceAddress, i)){
+      Serial.print("Found device ");
+      Serial.print(i, DEC);
+      Serial.print(" with address: ");
+      printAddress(tempDeviceAddress);
+      Serial.println();
+    } else {
+      Serial.print("Found ghost device at ");
+      Serial.print(i, DEC);
+      Serial.print(" but could not detect address. Check power and cabling");
+    }
+  }
+}
+
+void loop(){ 
+  sensors.requestTemperatures(); // Send the command to get temperatures
+  
+  // Loop through each device, print out temperature data
+  for(int i=0;i<numberOfDevices; i++){
+    // Search the wire for address
+    if(sensors.getAddress(tempDeviceAddress, i)){
+      // Output the device ID
+      Serial.print("Temperature for device: ");
+      Serial.println(i,DEC);
+      // Print the data
+      float tempC = sensors.getTempC(tempDeviceAddress);
+
+      Serial.print("Temp C: ");
+      Serial.println(tempC);
+
+      display.clearDisplay();
+      display.println(tempC);
+
+      delay(1000);
+    }
+  }
+}
+
+
